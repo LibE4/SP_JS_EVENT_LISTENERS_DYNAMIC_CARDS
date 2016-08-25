90788b3da828246aa9c7663884a149320ea74f36
Bin Li <libin_75@hotmail.com>
2016-08-24 21:33:11 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Dynamic Cards
+
+## Description
+Practice to get, create, modify and style DOMchain array methods: sort, filter, map, reduce.
+
+## How to run
+In your terminal run:
+```
+http-server -p 9999
+```
+then navigate to http://localhost:9999 in your browser
+
+## Contributors
+- [Bin Li](https://github.com/LibE4)
+
---
+++ cards.js
@@ -0,0 +1,21 @@
+function createCards() {
+var buttonDelete = document.createElement("button");
+buttonDelete.innerHTML = "Delete";
+buttonDelete.addEventListener("click", function() {
+	this.parentNode.remove();
+})
+var textElement = document.createElement("div");
+textElement.innerHTML = textareaElement.value;
+var cardElement = document.createElement("div");
+cardElement.style.width = "300px";
+cardElement.style.height = "500px";
+cardElement.style.border = "1px solid black";
+cardElement.appendChild(buttonDelete);
+cardElement.appendChild(textElement);
+displayElement.appendChild(cardElement);
+}
+
+var textareaElement = document.getElementById("card-content");
+var buttonCreate = document.getElementById("create");
+var displayElement = document.getElementById("display-area");
+buttonCreate.addEventListener("click", createCards);
